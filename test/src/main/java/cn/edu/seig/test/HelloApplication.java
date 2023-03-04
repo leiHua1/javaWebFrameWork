cf84ec848db95b2baa9cd52e30d61a7e8f8ec1b8
洗洗睡吧 <1517921059@qq.com>
2023-03-04 05:42:31 +0000

    删除文件 test

--- test/src/main/java/cn/edu/seig/test/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.seig.test;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
